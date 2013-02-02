50f168af386f720466b72f673ec811f705f03635
qiantum <qiantum@fffe4a6e-fe96-11de-9748-a16e6a44286a>
2013-02-02 05:41:15 +0000

    file import / export

--- qutum-web/index.js
+++ qutum-web/index.js
@@ -6,7 +6,8 @@
 //
 (function(){
 
-var Z = Util.dom('#zonest'), ZT = Util.dom('#left > .tool'), E = Util.dom('.editor')
+var Z = Util.dom('#zonest'), ZT = Util.dom('#left > .tool')
+var E = Util.dom('.editor')
 var Zs = {}, Es = {}, Key, tooltest
 
 Tool()
@@ -30,10 +31,15 @@ function Zonest(key)
 	Util.text(Util.add(save, 'div'), 'Save')
 	Util.on(save, 'click', null, Save, [ key ])
 //	case -115: case -83: case -83.5
+	var exp = Util.add(t, 'a')
+	Util.text(exp, '{')
+	Util.text(Util.add(exp, 'div'), 'Export, Click and \n Save Link As...')
+	Util.on(exp, 'click', null, Export, [ key ], true)
 	var rem = Util.add(t, 'a')
 	Util.text(rem, 'x')
 	Util.text(Util.add(rem, 'div'), 'Remove')
 	Util.on(rem, 'click', null, Remove, [ key ])
+	z.exp = exp
 	return Z.appendChild(z)
 }
 
@@ -44,18 +50,30 @@ function Tool()
 	Util.text(Util.add(n, 'div'), 'New')
 	Util.on(n, 'click', null, New)
 	var save = Util.add(ZT, 'a')
-	Util.text(save, '<')
+	Util.text(save, '<<')
 	Util.text(Util.add(save, 'div'), 'Save all')
-	Util.on(save, 'click', null, SaveAll)
+	var imps = Util.add(ZT, 'input')
+	imps.type = 'file', imps.multiple = 'multiple'
+	imps.style.display = 'none'
+	var imp = Util.add(ZT, 'a')
+	Util.text(imp, '}}')
+	Util.text(Util.add(imp, 'div'), 'Import')
+	Util.on(imps, 'change', null, ImportAll, [ imps ])
+	Util.on(imp, 'click', imps, imps.click)
+	var exp = Util.add(ZT, 'a')
+	Util.text(exp, '{{')
+	Util.text(Util.add(exp, 'div'), 'Export all')
+	Util.on(exp, 'click', null, ExportAll)
 }
 
-function New()
+function New(Els)
 {
 	var key = Saver.New()
-	Zonest(key).scrollIntoView(), Load(key), Save(key)
+	Zonest(key).scrollIntoView(), Load(key, Els), Save(key)
+	return key
 }
 
-function Load(key)
+function Load(key, Els)
 {
 	if (Key)
 		Es[Key].dom.style.display = 'none', Zs[Key].className = ''
@@ -64,7 +82,7 @@ function Load(key)
 		e.dom.style.display = ''
 	else
 	{
-		var els = []
+		var els = Els || []
 		e = Es[key] = new Edit(Util.add(E, 'div', 'edit'), els)
 		Saver.load(key, e.zonest, els)
 		e.onUnsave = Unsave
@@ -97,18 +115,6 @@ function SaveAll()
 		Es[k].unsave && Save(key)
 }
 
-function Remove(key)
-{
-	var z = Zs[key], e = Es[key]
-	if ( !confirm('Remove ' + (Saver.Name(key) || 'unnamed') + ' ?'))
-		return
-	Saver.remove(key)
-	delete Es[key], delete Zs[key], Z.removeChild(z)
-	if (key == Key)
-		Key = null, E.removeChild(e.dom), tooltest = null,
-		Util.dom('.tool', E).innerHTML = Util.dom('.toolv', E).innerHTML = ''
-}
-
 function Unsave()
 {
 	for (var key in Es)
@@ -121,6 +127,61 @@ onbeforeunload = function ()
 	for (var key in Es)
 		if (Es[key].unsave)
 			return (Saver.Name(key) || 'unnamed') + ' unsaved !'
+}
+
+function Import(file)
+{
+	var els = [], key = New(els)
+	Filer.load(key, file, Es[key].zonest, els, function (ok, err)
+	{
+		if (ok)
+			Save(key)
+		else
+			Remove(key, true), alert('Import ' + file.name + ' Error: ' + err)
+	})
+}
+
+function ImportAll(files)
+{
+	var s = []
+	for (var i = 0; i < files.files.length; i++)
+		s[i] = files.files[i]
+	files.value = ''
+	for (var i = 0; i < s.length; i++)
+		Import(s[i])
+}
+
+function Export(key, forAll)
+{
+	var e = Es[key] || Load(key), z = Zs[key]
+	if (forAll || !z.exp.getAttribute('href'))
+		Filer.save(key, e.zonest, function (ok, enc)
+		{
+			if (ok)
+				z.exp.href = enc
+			else
+				alert('Export ' + Saver.Name(key) + ' Error: ' + enc)
+		})
+	else
+		z.exp.removeAttribute('href')
+}
+
+function ExportAll(key)
+{
+	for (var key in Zs)
+		Export(key, true) // TODO bug: no show
+}
+
+function Remove(key, force)
+{
+	var z = Zs[key], e = Es[key]
+	if ( !force && !confirm('Remove ' + (Saver.Name(key) || 'unnamed') + ' ?'))
+		return
+	Saver.remove(key)
+	delete Es[key], delete Zs[key], Z.removeChild(z)
+	if (key == Key)
+		Key = null, E.removeChild(e.dom), tooltest = null,
+		Util.dom('.tool', E).innerHTML = Util.dom('.toolv', E).innerHTML = ''
 }
 
 Util.dom('#split *').onclick = function ()
--- qutum-web/saver.js
+++ qutum-web/saver.js
@@ -41,38 +41,35 @@ Saver.New = function ()
 	return key
 }
 
-Saver.load = function (key, zonest, els)
-{
-	var s = localStorage.getItem(key)
-	if ( !s)
-		return
-	var name = Saver.Name(key)
-	s = new String(s), s.x = 0
-	if (s[s.x++] != '\u0a51') // 10 Q
-		throw 'unknown format'
-	els[0] = null
-	sdec(zonest, s, els)
-	if (s.x != s.length)
-		throw 'unexpected end'
-	zonest.Name(name)
-}
-
 Saver.save = function (key, zonest, onRename)
 {
 	var name = Saver.Name(key)
-	var s = [ '\u0a51' ] // Q 10
-	senc(zonest, s, {}, 0)
-	zonest.edit.unsave = 0
-	s = s.join('')
+	var enc = SaverEnc()
+	save(enc, zonest, {}, 0)
+	enc = enc.finish()
 	var rekey
 	if (zonest.name == name)
-		rekey = key, localStorage.setItem(key, s)
+		rekey = key, localStorage.setItem(key, enc)
 	else
 		rekey = Date.now() + '!' + zonest.name,
 		onRename && onRename(key, rekey),
-		localStorage.setItem(rekey, s),
+		localStorage.setItem(rekey, enc),
 		localStorage.removeItem(key)
+	zonest.edit.unsave = 0
 	return rekey
+}
+
+Saver.load = function (key, zonest, els)
+{
+	var dec = localStorage.getItem(key)
+	if ( !dec)
+		return
+	var name = Saver.Name(key)
+	els[0] = null
+	dec = SaverDec(dec)
+	load(dec, zonest, els)
+	dec.finish()
+	zonest.Name(name)
 }
 
 Saver.remove = function (key)
@@ -82,67 +79,52 @@ Saver.remove = function (key)
 }
 
 ////////////////////////////////       ////////////////////////////////
+//////////////////////////////// filer ////////////////////////////////
+////////////////////////////////       ////////////////////////////////
+
+Filer = function ()
+{
+}
+
+Filer.save = function (key, zonest, onFinish)
+{
+	Saver.Name(key)
+	var enc = FilerEnc()
+	save(enc, zonest, {}, 0)
+	enc = enc.finish(onFinish)
+}
+
+Filer.load = function (key, file, zonest, els, onFinish)
+{
+	Saver.Name(key)
+	var name = file.name, namedot = name.lastIndexOf('.')
+	namedot >= 0 && (name = name.substr(0, namedot))
+	var r = new FileReader()
+	r.onerror = function () { onFinish(false, r.error) }
+	r.onload = function ()
+	{
+		try
+		{
+			els[0] = null
+			var dec = FilerDec(r.result)
+			load(dec, zonest, els)
+			dec.finish()
+			zonest.Name(name)
+			onFinish(true)
+		}
+		catch (e)
+		{
+			onFinish(false, e)
+		}
+	}
+	r.readAsArrayBuffer(file)
+}
+
+////////////////////////////////       ////////////////////////////////
 //////////////////////////////// codec ////////////////////////////////
 ////////////////////////////////       ////////////////////////////////
 
-function sencN(s, n)
-	// Webkit localStorage can't save \0 and Firefox \ud800-\udfff \ufffe \uffff
-{
-	if (n >> 13 == n >> 31) // 14bit
-		s.push(String.fromCharCode((n & 0x3fff) + 1))
-	else if (n >> 27 == n >> 31) // 28bit
-		s.push(String.fromCharCode((n & 0x3fff) + 0x4001, (n >> 14 & 0x3fff) + 1))
-	else if (n == (n|0))
-		s.push(String.fromCharCode((n & 0x3fff) + 0x8001, (n >> 14 & 0x3fff) + 1, (n >> 28 & 15) + 1))
-	else
-		throw 'invalid number'
-}
-function sencS(s, str)
-{
-	var n = str.length
-	if (n >> 13 == n >> 31) // 14bit
-		s.push(String.fromCharCode((n & 0x3fff) + 1), str)
-	else if (n >> 27 == n >> 31) // 28bit
-		s.push(String.fromCharCode((n & 0x3fff) + 0x4001, (n >> 14 & 0x3fff) + 1), str)
-	else
-		throw 'string too long'
-}
-function sdecN(s)
-{
-	var x = s.x, a = s.charCodeAt(x), b, c
-	if (a <= 0x4000)
-		return s.x = x + 1, a-1 << 18 >> 18
-	else if ((b = s.charCodeAt(x + 1)) == b && a <= 0x8000)
-		return s.x = x + 2, b-1 << 18 >> 4 | a-0x4001
-	else if ((c = s.charCodeAt(x + 2)) == c)
-		if (a <= 0xc000)
-			return s.x = x + 3, c-1 << 28 | (b-1 & 0x3fff) << 14 | a-0x8001
-		else
-			throw 'invalid number'
-	throw 'eof'
-}
-function sdecN14(s)
-{
-	return s.charCodeAt(s.x)-1 << 18 >> 18
-}
-function sdecS(s)
-{
-	var x = s.x, a = s.charCodeAt(x)
-	if (a < 0x4000)
-		x = x + 1, a = a-1
-	else if ((b = s.charCodeAt(x + 1)) == b)
-		if (a <= 0x8000)
-			x = x + 2, a = b-1 << 18 >> 4 | a-0x4001
-		else
-			throw 'string too long'
-	else
-		throw 'eof'
-	if ((s.x = x + a) > s.length)
-		throw 'eof'
-	return s.substr(x, a)
-}
-
-function senc(d, s, us, el)
+function save(enc, d, us, el)
 {
 	d.el = ++el
 	var uel = 0
@@ -150,56 +132,56 @@ function senc(d, s, us, el)
 		uel = d.unity;
 	else if (d.io && d.uNext != d)
 		(uel = us[d.unity]) || (us[d.unity] = el)
-	sencN(s, (d.tv < 0 ? 8 : d.tv ? 16 : 0) | (uel && 32) |
+	enc.num((d.tv < 0 ? 8 : d.tv ? 16 : 0) | (uel && 32) |
 		(d.io < 0 ? 1 : d.io > 0 ? 3 : d.row && d == d.row[0] ? 34 : 2))
 	if (uel)
-		sencN(s, uel)
+		enc.num(uel)
 	else
-		sencS(s, d.zone ? d.name : '')
+		enc.str(d.zone ? d.name : '')
 	for (var R = 0, r; r = d.rows[R]; R++)
 		for (var D = 0, dd; dd = r[D]; D++)
-			dd.yield || dd.layer || (el = senc(dd, s, us, el))
+			dd.yield || dd.layer || (el = save(enc, dd, us, el))
 	for (var W = 0, w; w = d.ws[W]; W++)
 		if ( !w.yield)
-			sencN(s, 4), sencN(s, w.base.el), sencN(s, w.agent.el)
-	sencN(s, 0)
+			enc.num(4), enc.num(w.base.el), enc.num(w.agent.el)
+	enc.num(0)
 	return el
 }
-function sdec(d, s, els)
+function load(dec, d, els)
 {
 	d.el = els.push(d)
-	var x = sdecN(s), u
-	d.Tv(x & 8 ? -1 : x & 16 ? 1 : 0)
-	if (d.io == 0 || ~x & 32)
+	var q = dec.num(), u
+	d.Tv(q & 8 ? -1 : q & 16 ? 1 : 0)
+	if (d.io == 0 || ~q & 32)
 	{
-		var name = sdecS(s)
+		var name = dec.str()
 		if (d.zone)
 			d.Name(name)
 		else if (name)
 			throw 'invalid name'
 	}
-	else if (u = els[sdecN(s)])
+	else if (u = els[dec.num()])
 		d.unityTo(u)
 	else
 		throw 'invalid unity'
-	for (var X = x = xx = 0; x = (xx = sdecN14(s)) & 7; X = x)
-		if (x < X)
+	for (var Q = q = xx = 0; q = (xx = dec.peek()) & 7; Q = q)
+		if (q < Q)
 			throw 'invalid format'
-		else if (x == 1)
-			sdec(new Datum(-1).addTo(d, 0, d.ox < 0 ? 0 : d.rows[0].length), s, els)
-		else if (x == 2)
+		else if (q == 1)
+			load(dec, new Datum(-1).addTo(d, 0, d.ox < 0 ? 0 : d.rows[0].length), els)
+		else if (q == 2)
 			if (xx & 32)
-				sdec(new Datum(0).addTo(d, d.ox < 0 ? 1 : d.ox, -1), s, els)
+				load(dec, new Datum(0).addTo(d, d.ox < 0 ? 1 : d.ox, -1), els)
 			else
-				sdec(new Datum(0).addTo(d,
-					d.ox <= 1 ? 1 : d.ox - 1, d.ox <= 1 ? 0 : d.rows[d.ox - 1].length), s, els)
-		else if (x == 3)
-			sdec(new Datum(1).addTo(d,
-				d.ox < 0 ? 1 : d.ox, d.ox < 0 ? 0 : d.rows[d.ox].length), s, els)
-		else if (x == 4)
+				load(dec, new Datum(0).addTo(d,
+					d.ox <= 1 ? 1 : d.ox - 1, d.ox <= 1 ? 0 : d.rows[d.ox - 1].length), els)
+		else if (q == 3)
+			load(dec, new Datum(1).addTo(d,
+				d.ox < 0 ? 1 : d.ox, d.ox < 0 ? 0 : d.rows[d.ox].length), els)
+		else if (q == 4)
 		{
-			sdecN(s)
-			var b = els[sdecN(s)], a = els[sdecN(s)]
+			dec.num()
+			var b = els[dec.num()], a = els[dec.num()]
 			if ( !b || !a || b == a)
 				throw 'invalid wire'
 			if (b.agent(new Wire, a))
@@ -207,7 +189,158 @@ function sdec(d, s, els)
 		}
 		else
 			throw 'invalid format'
-	sdecN(s)
+	dec.num()
+	return q
+}
+
+////////////////////////////////       ////////////////////////////////
+//////////////////////////////// codec ////////////////////////////////
+////////////////////////////////       ////////////////////////////////
+
+function SaverEnc()
+{
+	var s = [ '\u0a51' ], x = 1 // 10 Q
+	s.num = function (n) // Webkit localStorage denies \0 and Firefox \ud800-\udfff \ufffe \uffff
+	{
+		if (n != (n|0))
+			throw 'invalid number'
+		else if (n >> 13 == n >> 31) // 14bit signed
+			s[x++] = String.fromCharCode((n & 0x3fff) + 1)
+		else if (n >> 27 == n >> 31) // 28bit signed
+			s[x++] = String.fromCharCode((n & 0x3fff) + 0x4001, (n >> 14 & 0x3fff) + 1)
+		else // 32bit signed
+			s[x++] = String.fromCharCode((n & 0x3fff) + 0x8001, (n >> 14 & 0x3fff) + 1,
+				(n >> 28 & 15) + 1)
+	}
+	s.str = function (str)
+	{
+		var n = str.length
+		if (n >> 13 == 0) // 13bit unsigned
+			s[x++] = String.fromCharCode((n & 0x3fff) + 1)
+		else if (n >> 27 == 0) // 27bit unsigned
+			s[x++] = String.fromCharCode((n & 0x3fff) + 0x4001, (n >> 14 & 0x3fff) + 1)
+		else
+			throw 'string too long'
+		s[x++] = str
+	}
+	s.finish = function ()
+	{
+		return s.join('')
+	}
+	return s
+}
+
+function SaverDec(s)
+{
+	s = new String(s)
+	if (s[0] != '\u0a51') // 10 Q
+		throw 'unknown format'
+	var n = s.length, x = 1
+	s.num = function ()
+	{
+		var a = s.charCodeAt(x), b, c
+		if (a <= 0x4000)
+			return x += 1, a-1 << 18 >> 18
+		else if ((b = s.charCodeAt(x + 1)) == b && a <= 0x8000)
+			return x += 2, b-1 << 18 >> 4 | a-0x4001
+		else if ((c = s.charCodeAt(x + 2)) == c)
+			if (a <= 0xc000)
+				return x += 3, c-1 << 28 | (b-1 & 0x3fff) << 14 | a-0x8001
+			else
+				throw 'invalid number'
+		throw 'eof'
+	}
+	s.peek = function ()
+	{
+		return s.charCodeAt(x)-1 << 18 >> 18
+	}
+	s.str = function ()
+	{
+		var a = s.charCodeAt(x)
+		if (a < 0x4000)
+			x = x + 1, a = a-1
+		else if ((b = s.charCodeAt(x + 1)) == b)
+			if (a <= 0x8000)
+				x = x + 2, a = b-1 << 14 | a-0x4001
+			else
+				throw 'string too long'
+		else
+			throw 'eof'
+		if ((x += a) > n)
+			throw 'eof'
+		return s.substr(x - a, a)
+	}
+	s.finish = function ()
+	{
+		if (x != n)
+			throw 'unexpected end'
+	}
+	return s
+}
+
+function FilerEnc() // only for little endian
+{
+	var s = [], x = 0
+	s[x++] = 0x0a51 // 10 Q
+	s.num = function (n)
+	{
+		if (n != (n|0)) // 32bit signed
+			throw 'invalid number'
+		s[x++] = n, s[x++] = n >> 16
+	}
+	s.str = function (str)
+	{
+		var n = str.length
+		if (n >> 27 != 0) // 27bit unsigned
+			throw 'string too long'
+		s[x++] = n, s[x++] = n >> 16
+		for (var i = 0; i < n; i++)
+			s[x++] = str.charCodeAt(i)
+	}
+	s.finish = function (onFinish)
+	{
+		var r = new FileReader()
+		r.onerror = function () { onFinish(false, r.error) }
+		r.onload = function () { onFinish(true, r.result) }
+		r.readAsDataURL(new Blob([ new Uint16Array(s) ], { type:'application/qutum-file' }))
+	}
+	return s
+}
+
+function FilerDec(bytes) // only for little endian
+{
+	var n = bytes.byteLength
+	if (n % 2)
+		throw 'unknown format'
+	var s = new Uint16Array(bytes), x = 1
+	if (s[0] != 0x0a51) // 10 Q
+		throw 'unknown format'
+	s.num = function ()
+	{
+		x += 2
+		return s[x-1] << 16 | s[x-2]
+	}
+	s.peek = function ()
+	{
+		return s[x+1] << 16 | s[x]
+	}
+	s.str = function ()
+	{
+		x += 2
+		var v = s[x-1] << 16 | s[x-2]
+		if (v >> 27 != 0)
+			throw 'string too long'
+		x += v
+		if (x + x > n)
+			throw 'eof'
+		return String.fromCharCode.apply(null, new Uint16Array(bytes, x+x-v-v, v+v))
+	}
+	s.finish = function ()
+	{
+		if (x + x != n)
+			throw 'unexpected end'
+	}
+	return s
 }
 
 })()
